7062e11d4b852dbd64f9849493c13281524b25a6
Daniela Clivio <danielaclivio@Danielas-MacBook-Pro.local>
2019-04-09 11:52:06 -0500

    S11C2 Casa archivos finales

--- primos.cpp
+++ primos.cpp
@@ -3,29 +3,28 @@ using namespace std;
 
 int primos(int n1,int n2){
   int n=0;
-  for(int j=2;j<=n2;j++){
+  for (int j=n1;j<=n2;j++){
     int contador=0;
-    for(int i=n1;i<=n2;i++){
+    for(int i=2;i<=(j/2);i++){
       if(j%i==0){
 	contador++;
-      if (contador==2){
-	n=j;
-	cout<<j<<endl;
+	break;
       }
-      }}
-    contador=0;
-    
+    }
+    if(contador==0 && j!=1){
+      n=j;
+      cout<<j<<endl;
+    }
   }
   return n;
 }
 
 int main(){
-  int n1;
-  cout<<"Ingrese el número menor de su intervalo: "<<endl;
+  int n1,n2;
+  cout<<"Ingrese el numero menor de su intervalo:"<<endl;
   cin>>n1;
-  int n2;
-  cout<<"Ingrese el número mayor de su intervalo: "<<endl;
+  cout<<"Ingrese el numero mayor de su intervalo:"<<endl;
   cin>>n2;
-  cout<<"Los números primos en el intervalo dado son ="<<endl;
+  cout<<"Los primos son= "<<endl;
   primos(n1,n2);
 }
